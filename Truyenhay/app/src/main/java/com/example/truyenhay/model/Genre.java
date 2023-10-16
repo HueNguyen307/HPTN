6ee315d582c7401dd4fb041ffc63a01493dee8e1
HueNguyen307 <93471792+HueNguyen307@users.noreply.github.com>
2023-10-16 09:55:06 +0700

    Repo

---
+++ Truyenhay/app/src/main/java/com/example/truyenhay/model/Genre.java
@@ -0,0 +1,22 @@
+package com.example.truyenhay.model;
+
+import java.io.Serializable;
+
+public class Genre implements Serializable {
+    String name;
+
+    public Genre(String name) {
+        this.name = name;
+    }
+
+    public Genre() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
